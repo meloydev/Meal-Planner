51cec96e3809f0c1a505b911cdbb1d35dc077b31
michael <michaelmeloy@outlook.com>
2016-10-29 00:45:52 -0500

    ready

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,9 +2,7 @@ const electron = require('electron');
 const {ipcRenderer} = electron;
 
 $(document).ready(function () {
-    //$('#btnClose').click(click.close);
-    //$('#btnMeal').click(click.navigate);
-    //$('#btnAddClient').click(click.navigate);
+    $('#btnClose').click(click.close); 
     $('[data-location]').click(click.navigate);
 })
 
